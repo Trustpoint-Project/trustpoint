fe82195804cc941ebc1e28adcf3deae903cc9c17
AlexHx8472 <alex_mail@posteo.de>
2025-03-03 16:26:36 +0100

    ruff & mypy check

--- trustpoint/devices/models.py
+++ trustpoint/devices/models.py
@@ -1,19 +1,24 @@
+"""This modules contains all models specific to the device abstractions."""
+
 from __future__ import annotations
 
 import datetime
 import logging
 import secrets
+from typing import TYPE_CHECKING
 
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-from django.contrib.contenttypes.models import ContentType
-from util.field import UniqueNameValidator
-from trustpoint_core import oid
-
-from pki.models import CertificateModel, DomainModel, CredentialModel, IssuingCaModel
+from django.utils.translation import gettext_lazy as _
 from pki.models.credential import CredentialModel
+from pki.models.domain import DomainModel
 from pki.models.truststore import TruststoreModel
+from pyasn1_modules.rfc3280 import common_name  # type: ignore[import-untyped]
+from trustpoint_core import oid  # type: ignore[import-untyped]
+from util.field import UniqueNameValidator
+
+if TYPE_CHECKING:
+    from typing import Any
 
 logger = logging.getLogger(__name__)
 
@@ -21,15 +26,13 @@ __all__ = [
     'DeviceModel',
     'IssuedCredentialModel',
     'RemoteDeviceCredentialDownloadModel',
-
 ]
 
+
 class DeviceModel(models.Model):
+    """The DeviceModel."""
 
     objects: models.Manager[DeviceModel]
-
-    def __str__(self) -> str:
-        return f'DeviceModel(unique_name={self.unique_name})'
 
     id = models.AutoField(primary_key=True)
     unique_name = models.CharField(
@@ -37,22 +40,15 @@ class DeviceModel(models.Model):
     )
     serial_number = models.CharField(_('Serial-Number'), max_length=100)
     domain = models.ForeignKey(
-        DomainModel,
-        verbose_name=_('Domain'),
-        related_name='devices',
-        blank=True,
-        null=True,
-        on_delete=models.PROTECT
+        DomainModel, verbose_name=_('Domain'), related_name='devices', blank=True, null=True, on_delete=models.PROTECT
     )
 
     domain_credential_onboarding = models.BooleanField(
-        verbose_name=_('Domain Credential Onboarding'),
-        default=True,
-        blank=False,
-        null=False
+        verbose_name=_('Domain Credential Onboarding'), default=True, blank=False, null=False
     )
 
     class OnboardingStatus(models.IntegerChoices):
+        """The onboarding status."""
 
         NO_ONBOARDING = 0, _('No Onboarding')
         PENDING = 1, _('Pending')
@@ -66,6 +62,7 @@ class DeviceModel(models.Model):
     )
 
     class OnboardingProtocol(models.IntegerChoices):
+        """Choices of onboarding protocols."""
 
         NO_ONBOARDING = 0, _('No Onboarding')
         EST_PASSWORD = 1, _('EST - Username & Password')
@@ -79,9 +76,11 @@ class DeviceModel(models.Model):
         choices=OnboardingProtocol,
         verbose_name=_('Onboarding Protocol'),
         null=False,
-        default=OnboardingProtocol.NO_ONBOARDING)
+        default=OnboardingProtocol.NO_ONBOARDING,
+    )
 
     class PkiProtocol(models.IntegerChoices):
+        """Choices of pki protocols."""
 
         MANUAL = 0, _('Manual Download')
         EST_PASSWORD = 1, _('EST - Username & Password')
@@ -90,59 +89,58 @@ class DeviceModel(models.Model):
         CMP_CLIENT_CERTIFICATE = 4, _('CMP - LDevID')
 
     pki_protocol = models.IntegerField(
-        choices=PkiProtocol,
-        verbose_name=_('Pki Protocol'),
-        null=False,
-        default=PkiProtocol.MANUAL
+        choices=PkiProtocol, verbose_name=_('Pki Protocol'), null=False, default=PkiProtocol.MANUAL
     )
 
-
-    @property
-    def est_username(self) -> str:
-        return self.unique_name
-
-    est_password = models.CharField(
-        verbose_name=_('EST Password'),
-        max_length=128,
-        null=True,
-        blank=True,
-        default=None)
-    cmp_shared_secret = models.CharField(
-        verbose_name=_('CMP Shared Secret'),
-        max_length=128,
-        null=True,
-        blank=True,
-        default=None)
+    est_password = models.CharField(verbose_name=_('EST Password'), max_length=128, blank=True, default='')
+    cmp_shared_secret = models.CharField(verbose_name=_('CMP Shared Secret'), max_length=128, blank=True, default='')
 
     idevid_trust_store = models.ForeignKey(
         TruststoreModel,
         verbose_name=_('IDevID Manufacturer Truststore'),
         null=True,
         blank=True,
-        on_delete=models.DO_NOTHING)
+        on_delete=models.DO_NOTHING,
+    )
 
     created_at = models.DateTimeField(verbose_name=_('Created'), auto_now_add=True)
 
+    def __str__(self) -> str:
+        """Returns a human-readable string representation."""
+        return f'DeviceModel(unique_name={self.unique_name})'
+
+    @property
+    def est_username(self) -> str:
+        """Gets the EST username."""
+        return self.unique_name
+
     @property
     def signature_suite(self) -> oid.SignatureSuite:
+        """Gets the corresponding SignatureSuite object."""
         return oid.SignatureSuite.from_certificate(
-            self.domain.issuing_ca.credential.get_certificate_serializer().as_crypto())
+            self.domain.issuing_ca.credential.get_certificate_serializer().as_crypto()
+        )
 
     @property
     def public_key_info(self) -> oid.PublicKeyInfo:
+        """Gets the corresponding PublicKeyInfo object."""
         return self.signature_suite.public_key_info
 
 
 class IssuedCredentialModel(models.Model):
     """Model for all credentials and certificates that have been issued or requested by the Trustpoint."""
 
-    objects: models.Manager['IssuedCredentialModel']
+    objects: models.Manager[IssuedCredentialModel]
 
     class IssuedCredentialType(models.IntegerChoices):
+        """The type of the credential."""
+
         DOMAIN_CREDENTIAL = 0, _('Domain Credential')
         APPLICATION_CREDENTIAL = 1, _('Application Credential')
 
     class IssuedCredentialPurpose(models.IntegerChoices):
+        """The purpose of the issued credential."""
+
         DOMAIN_CREDENTIAL = 0, _('Domain Credential')
         GENERIC = 1, _('Generic')
         TLS_CLIENT = 2, _('TLS-Client')
@@ -150,46 +148,38 @@ class IssuedCredentialModel(models.Model):
         OPCUA_CLIENT = 4, _('OpcUa-Client')
         OPCUA_SERVER = 5, _('OpcUa-Server')
 
-
-
     id = models.AutoField(primary_key=True)
 
     common_name = models.CharField(verbose_name=_('Common Name'), max_length=255)
-    issued_credential_type = models.IntegerField(
-        choices=IssuedCredentialType,
-        verbose_name=_('Credential Type'))
+    issued_credential_type = models.IntegerField(choices=IssuedCredentialType, verbose_name=_('Credential Type'))
     issued_credential_purpose = models.IntegerField(
-        choices=IssuedCredentialPurpose,
-        verbose_name=_('Credential Purpose'))
+        choices=IssuedCredentialPurpose, verbose_name=_('Credential Purpose')
+    )
     credential = models.OneToOneField(
         CredentialModel,
         verbose_name=_('Credential'),
         on_delete=models.PROTECT,
         related_name='issued_credential',
         null=False,
-        blank=False
+        blank=False,
     )
     device = models.ForeignKey(
-        DeviceModel,
-        verbose_name=_('Device'),
-        on_delete=models.PROTECT,
-        related_name='issued_credentials'
+        DeviceModel, verbose_name=_('Device'), on_delete=models.PROTECT, related_name='issued_credentials'
     )
     domain = models.ForeignKey(
-        DomainModel,
-        verbose_name=_('Domain'),
-        on_delete=models.PROTECT,
-        related_name='issued_credentials'
+        DomainModel, verbose_name=_('Domain'), on_delete=models.PROTECT, related_name='issued_credentials'
     )
 
     created_at = models.DateTimeField(verbose_name=_('Created'), auto_now_add=True)
 
     def __str__(self) -> str:
-        return f'IssuedCredentialModel()'
+        """Returns a human-readable string representation."""
+        return f'IssuedCredentialModel(common_name={common_name})'
 
 
 class RemoteDeviceCredentialDownloadModel(models.Model):
     """Model to associate a credential model with an OTP and token for unauthenticated remoted download."""
+
     BROWSER_MAX_OTP_ATTEMPTS = 3
     TOKEN_VALIDITY = datetime.timedelta(minutes=3)
 
@@ -204,20 +194,31 @@ class RemoteDeviceCredentialDownloadModel(models.Model):
         """Return a string representation of the model."""
         return f'RemoteDeviceCredentialDownloadModel(credential={self.issued_credential_model.id})'
 
-    def save(self, *args: dict, **kwargs: dict) -> None:
+    def save(self, *args: Any, **kwargs: Any) -> None:
         """Generates a new random OTP on initial save of the model."""
         if not self.otp:
             self.otp = secrets.token_urlsafe(8)
         super().save(*args, **kwargs)
 
     def get_otp_display(self) -> str:
-        """Return the OTP in the format 'credential_id.otp' for display within the admin view."""
+        """Return the OTP in the format 'credential_id.otp' for display within the admin view.
+
+        Returns:
+            The str to display.
+        """
         if not self.otp or self.otp == '-':
             return 'OTP no longer valid'
         return f'{self.issued_credential_model.id}.{self.otp}'
 
     def check_otp(self, otp: str) -> bool:
-        """Check if the provided OTP matches the stored OTP."""
+        """Check if the provided OTP matches the stored OTP.
+
+        Args:
+            otp: The OTP to check.
+
+        Returns:
+            True if the OTP is valid, False otherwise.
+        """
         if not self.otp or self.otp == '-':
             return False
         matches = otp == self.otp
@@ -249,7 +250,14 @@ class RemoteDeviceCredentialDownloadModel(models.Model):
         return True
 
     def check_token(self, token: str) -> bool:
-        """Check if the provided token matches the stored token and whether it is still valid."""
+        """Check if the provided token matches the stored token and whether it is still valid.
+
+        Args:
+            token: The token to check.
+
+        Returns:
+            True if the token is valid, false otherwise.
+        """
         if not self.download_token or not self.token_created_at:
             return False
         if timezone.now() - self.token_created_at > self.TOKEN_VALIDITY:
