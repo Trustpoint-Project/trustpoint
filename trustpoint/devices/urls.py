1b4321fd28638752ee7801bcf63c40c36f1f2f1c
AlexHx8472 <alex_mail@posteo.de>
2025-03-03 16:14:11 +0100

    ruff & mypy pass

--- trustpoint/devices/urls.py
+++ trustpoint/devices/urls.py
@@ -1,6 +1,6 @@
 """URL configuration for the devices' application."""
 
-from django.urls import path  # type: ignore[import-untyped]
+from django.urls import path
 
 from . import views
 
@@ -12,78 +12,74 @@ urlpatterns = [
     path('details/<int:pk>/', views.DeviceDetailsView.as_view(), name='details'),
     path('configure/<int:pk>/', views.DeviceConfigureView.as_view(), name='config'),
     path('browser/', views.DeviceOnboardingBrowserLoginView.as_view(), name='browser_login'),
-    path('browser/credential-download/<int:pk>/',
-         views.DeviceBrowserCredentialDownloadView.as_view(),
-         name='browser_domain_credential_download'),
     path(
-        'credential-download/browser/<int:pk>/',
-        views.DeviceBrowserOnboardingOTPView.as_view(),
-        name='browser_otp_view'),
+        'browser/credential-download/<int:pk>/',
+        views.DeviceBrowserCredentialDownloadView.as_view(),
+        name='browser_domain_credential_download',
+    ),
+    path(
+        'credential-download/browser/<int:pk>/', views.DeviceBrowserOnboardingOTPView.as_view(), name='browser_otp_view'
+    ),
     path(
         'credential-download/browser/<int:pk>/cancel',
         views.DeviceBrowserOnboardingCancelView.as_view(),
-        name='browser_cancel'),
-    path(
-        'download/<int:pk>/',
-        views.DownloadPageDispatcherView.as_view(),
-        name='download'
+        name='browser_cancel',
     ),
+    path('download/<int:pk>/', views.DownloadPageDispatcherView.as_view(), name='download'),
     path(
-        'credential/download/<int:pk>/',
-        views.DeviceManualCredentialDownloadView.as_view(),
-        name='credential-download'
+        'credential/download/<int:pk>/', views.DeviceManualCredentialDownloadView.as_view(), name='credential-download'
     ),
-    path(
-        'certificate/download/<int:pk>/',
-        views.CertificateDownloadView.as_view(),
-        name='certificate-download'
-    ),
+    path('certificate/download/<int:pk>/', views.CertificateDownloadView.as_view(), name='certificate-download'),
     path(
         'certificate-lifecycle-management/<int:pk>/',
         views.DeviceCertificateLifecycleManagementSummaryView.as_view(),
-        name='certificate_lifecycle_management'),
+        name='certificate_lifecycle_management',
+    ),
     path(
         'certificate-lifecycle-management/<int:pk>/issue-tls-client-credential/',
         views.DeviceIssueTlsClientCredential.as_view(),
-        name='certificate_lifecycle_management-issue_tls_client_credential'),
+        name='certificate_lifecycle_management-issue_tls_client_credential',
+    ),
     path(
         'certificate-lifecycle-management/<int:pk>/issue-tls-server-credential/',
         views.DeviceIssueTlsServerCredential.as_view(),
-        name='certificate_lifecycle_management-issue_tls_server_credential'),
+        name='certificate_lifecycle_management-issue_tls_server_credential',
+    ),
     path(
         'certificate-lifecycle-management/<int:pk>/issue-opcua-client-credential/',
         views.DeviceIssueOpcUaClientCredential.as_view(),
-        name='certificate_lifecycle_management-issue_opcua_client_credential'),
+        name='certificate_lifecycle_management-issue_opcua_client_credential',
+    ),
     path(
         'certificate-lifecycle-management/<int:pk>/issue-opcua-server-credential/',
         views.DeviceIssueOpcUaServerCredential.as_view(),
-        name='certificate_lifecycle_management-issue_opcua_server_credential'),
-
-    path('certificate-lifecycle-management/<int:pk>/revoke/<int:credential_pk>/',
-         views.DeviceCredentialRevocationView.as_view(),
-         name='credential_revocation'),
-    path('revoke/<int:pk>/',
-         views.DeviceRevocationView.as_view(),
-         name='device_revocation'),
-    path(
-        'help/dispatch/<int:pk>/',
-        views.HelpDispatchView.as_view(),
-        name='help_dispatch'
+        name='certificate_lifecycle_management-issue_opcua_server_credential',
     ),
+    path(
+        'certificate-lifecycle-management/<int:pk>/revoke/<int:credential_pk>/',
+        views.DeviceCredentialRevocationView.as_view(),
+        name='credential_revocation',
+    ),
+    path('revoke/<int:pk>/', views.DeviceRevocationView.as_view(), name='device_revocation'),
+    path('help/dispatch/<int:pk>/', views.HelpDispatchView.as_view(), name='help_dispatch'),
     path(
         'help/no-onboarding/cmp-shared-secret/<int:pk>/',
         views.NoOnboardingCmpSharedSecretHelpView.as_view(),
-        name='help_no-onboarding_cmp-shared-secret'),
+        name='help_no-onboarding_cmp-shared-secret',
+    ),
     path(
         'help/onboarding/cmp-shared-secret/<int:pk>/',
         views.OnboardingCmpSharedSecretHelpView.as_view(),
-        name='help-onboarding_cmp-shared-secret'),
+        name='help-onboarding_cmp-shared-secret',
+    ),
     path(
         'help/onboarding/cmp-idevid/<int:pk>/',
         views.OnboardingCmpIdevidHelpView.as_view(),
-        name='help-onboarding_cmp-idevid'),
+        name='help-onboarding_cmp-idevid',
+    ),
     path(
         'help/onboarding/cmp-idevid-registration/<int:pk>/',
         views.OnboardingIdevidRegistrationHelpView.as_view(),
-        name='help-onboarding_cmp-idevid-registration')
+        name='help-onboarding_cmp-idevid-registration',
+    ),
 ]
