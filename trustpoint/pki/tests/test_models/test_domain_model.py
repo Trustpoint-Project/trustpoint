8fcc02cf5b93f9d7f3390401c96d5dad00b43dd9
Christian Schwinne <cschwinne@gmail.com>
2025-03-26 10:34:41 +0100

    Add additional tests for model deletion

--- trustpoint/pki/tests/test_models/test_domain_model.py
+++ trustpoint/pki/tests/test_models/test_domain_model.py
@@ -1,47 +1,20 @@
+"""Test the DomainModel class."""
+
 import datetime
 from typing import Any
 
 import pytest
 from cryptography import x509
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from django.utils import timezone
 from trustpoint_core import oid
 
 from pki.models import DomainModel, IssuingCaModel
-from pki.util.x509 import CertificateGenerator
 
 COMMON_NAME = 'Root CA'
 UNIQUE_NAME = COMMON_NAME.replace(' ', '_').lower()
 CA_TYPE = IssuingCaModel.IssuingCaTypeChoice.LOCAL_UNPROTECTED
 
 DOMAIN_UNIQUE_NAME = 'domain_name'
-
-
-@pytest.fixture
-def issuing_ca_instance() -> dict[str, Any]:
-    cert, priv_key = CertificateGenerator.create_root_ca(cn=COMMON_NAME)
-    issuing_ca = CertificateGenerator.save_issuing_ca(
-        issuing_ca_cert=cert, private_key=priv_key, chain=[], unique_name=UNIQUE_NAME, ca_type=CA_TYPE
-    )
-    return {'issuing_ca': issuing_ca, 'cert': cert, 'priv_key': priv_key}
-
-
-@pytest.fixture
-def domain_instance(issuing_ca_instance: dict[str, Any]) -> dict[str, Any]:
-    """Fixture for a DomainModel instance using a valid issuing CA."""
-    issuing_ca = issuing_ca_instance.get('issuing_ca')
-    priv_key = issuing_ca_instance.get('priv_key')
-    cert = issuing_ca_instance.get('cert')
-    if (
-        not isinstance(issuing_ca, IssuingCaModel)
-        or not isinstance(cert, x509.Certificate)
-        or not isinstance(priv_key, RSAPrivateKey)
-    ):
-        msg = 'Issuig CA not created properly'
-        raise TypeError(msg)
-    domain = DomainModel.objects.create(unique_name=DOMAIN_UNIQUE_NAME, issuing_ca=issuing_ca, is_active=True)
-    issuing_ca_instance.update({'domain': domain})
-    return issuing_ca_instance
 
 
 def test_attributes_and_properties(domain_instance: dict[str, Any]) -> None:
