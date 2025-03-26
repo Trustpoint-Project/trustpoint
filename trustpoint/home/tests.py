61de5c714d54395c770dd0cade27e966d4718ea1
Rohit Bohara <r.bohara@asvin.io>
2025-03-26 11:50:55 +0100

    fix mypy and ruff errors

--- trustpoint/home/tests.py
+++ trustpoint/home/tests.py
@@ -1,91 +1,86 @@
 """Contains test cases to ensure the functionality and correctness of the Home application."""
 
+from http import HTTPStatus
+
 import pytest
-from django.urls import reverse
-from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
+from devices.models import DeviceModel
+from django.contrib.auth.models import User
 from django.core.management import call_command
+from django.test import Client
+from django.urls import reverse
+
 from .models import NotificationModel
 
+
 @pytest.fixture
-def dashboard_response(client):
-    user = User.objects.create_user(username="admin", password="testing321")
+def setup_client(client: Client) -> Client:
+    """Fixture to run the management command to setup notifications."""
+    user = User.objects.create_user(username='admin', password='testing321')
     client.force_login(user)
-    response = client.get(reverse("home:dashboard"))
-    return response
-
+    return client
 
 @pytest.fixture
-def setup_notifications():
-    """Fixture to run the setup notification management command before tests."""
-
-    call_command("trustpoint_setup_notifications")
+def setup_notifications() -> NotificationModel | None:
+    """Fixture to run the management command to setup notifications."""
+    call_command('trustpoint_setup_notifications')
     return NotificationModel.objects.first()
 
 @pytest.fixture
-def notification_response(client):
-    """Fixture to get notification details for the first notification."""
-
-    user = User.objects.create_user(username="admin", password="testing321")
-    client.force_login(user)
-    response = client.get(reverse("home:notification_details", args=[1]))
-    return response
+def setup_domain_and_devices() -> DeviceModel | None:
+    """Fixture to run the management command to setup domain and devices."""
+    call_command('add_domains_and_devices')
+    return DeviceModel.objects.first()
 
 @pytest.mark.django_db
-def test_notification_details_view(setup_notifications, notification_response):
+def test_dashboard_view(setup_client: Client) -> None:
+    """Test that the dashboard page is accessible and contain table and charts."""
+    response = setup_client.get(reverse('home:dashboard'))
+    assert response.status_code == HTTPStatus.OK, 'dashboard page is not accessible'
 
-    """Test that the notification details page is accessible for a logged-in user."""
-    assert setup_notifications is not None, "No notification was created by the command"
-    assert notification_response.status_code == 200
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-@pytest.mark.django_db
-def test_dashboard_view(dashboard_response):
-    """Test that the dashboard page is accessible for a logged-in user."""
+    table = soup.find('table', {'class': 'table'})
 
-    assert dashboard_response.status_code == 200
-    assert "Summary" in dashboard_response.content.decode()
-    assert "Certificates" in dashboard_response.content.decode()
-    assert "Notifications" in dashboard_response.content.decode()
-    assert "Filter" in dashboard_response.content.decode()
-    assert "Charts" in dashboard_response.content.decode()
-    assert "Device" in dashboard_response.content.decode()
-
-@pytest.mark.django_db
-def test_notification_table(dashboard_response):
-    """Test that the dashboard contains the notification table."""
-
-    assert dashboard_response.status_code == 200
-
-    soup = BeautifulSoup(dashboard_response.content, "html.parser")
-    table = soup.find("table", {"class": "table"})
-    
-    assert table is not None, "Dashboard table not found!"
+    assert table is not None, 'Dashboard table not found!'
 
     # Validate headers
-    headers = [th.text.strip() for th in table.find_all("th")]
-    expected_headers = ["Type", "Description", "Details"]
-    assert all(h in headers for h in expected_headers), "Missing table headers"
-
-@pytest.mark.django_db
-def test_charts(dashboard_response):
-    """Test that the dashboard contains the notification table."""
-
-    assert dashboard_response.status_code == 200
-
-    soup = BeautifulSoup(dashboard_response.content, "html.parser")
+    headers = [th.text.strip() for th in table.find_all('th')]
+    expected_headers = ['Type', 'Description', 'Details']
+    assert all(h in headers for h in expected_headers), 'Missing table headers'
     # Check if divs with specific IDs exist
-    div_ids = ["deviceChartTab", "certChartTab", "caChartTab"]
+    div_ids = ['deviceChartTab', 'certChartTab', 'caChartTab']
 
     for canvas_id in div_ids:
-        div = soup.find("div", {"id": canvas_id})
+        div = soup.find('div', {'id': canvas_id})
         assert div is not None, f"Div with id '{canvas_id}' not found!"
 
     # Check if canvases with specific IDs exist
-    canvas_ids = ["devicesByOSLineChart", "devicesByDomainDonutChart", "devicesByOPBarChart", 
-        "certsByStatusLineChart", "certsByDomainPieChart", "certsByTemplateBarChart",
-        "certsByIssuingCADonutChart", "certsByDateStackChart", "issuingCAsByTypePieChart"
+    canvas_ids = ['devicesByOSLineChart', 'devicesByDomainDonutChart', 'devicesByOPBarChart',
+        'certsByStatusLineChart', 'certsByDomainPieChart', 'certsByTemplateBarChart',
+        'certsByIssuingCADonutChart', 'certsByDateStackChart', 'issuingCAsByTypePieChart'
         ]
 
     for canvas_id in canvas_ids:
-        div = soup.find("canvas", {"id": canvas_id})
+        div = soup.find('canvas', {'id': canvas_id})
         assert div is not None, f"canvas with id '{canvas_id}' not found!"
+
+@pytest.mark.django_db
+def test_notification_details_view(setup_notifications:NotificationModel, setup_client: Client) -> None:
+    """Test that the notification details page is accessible for a logged-in user."""
+    assert setup_notifications is not None, 'No notification was created by the command'
+    response = setup_client.get(reverse('home:notification_details', args=[1]))
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.django_db
+def test_dashboard_chart_view(setup_domain_and_devices:DeviceModel, setup_client: Client) -> None:
+    """Test that the dashboard chart data is accessible for a logged-in user."""
+    assert setup_domain_and_devices is not None, 'No domain and device were created by the command'
+    response = setup_client.get(reverse('home:dashboard_data'))
+    assert response.status_code == HTTPStatus.OK
+    dashboard_data = response.json()
+    required_keys = {'device_counts', 'cert_counts', 'issuing_ca_counts',
+        'device_counts_by_os', 'device_counts_by_op', 'cert_counts_by_status',
+        'cert_counts_by_issuing_ca', 'cert_counts_by_issuing_ca_and_date',
+        'ca_counts_by_type'}
+    assert required_keys.issubset(dashboard_data.keys()), f'Missing keys: {required_keys - dashboard_data.keys()}'
