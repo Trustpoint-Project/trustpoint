29849f50d30832c0074766b4adc25e8ce231ab2d
Christian Schwinne <cschwinne@gmail.com>
2025-04-15 16:32:09 +0200

    Make TLS server cert self-signed again

--- trustpoint/setup_wizard/tls_credential.py
+++ trustpoint/setup_wizard/tls_credential.py
@@ -27,105 +27,8 @@ class Generator:
     def _generate_key_pair() -> PrivateKeySerializer:
         return PrivateKeySerializer(ec.generate_private_key(curve=ec.SECP256R1()))
 
-    def _generate_root_ca(self) -> CredentialSerializer:
-        private_key_serializer = self._generate_key_pair()
-        private_key = private_key_serializer.as_crypto()
-        public_key = private_key_serializer.public_key_serializer.as_crypto()
-
-        builder = x509.CertificateBuilder()
-        builder = builder.subject_name(
-            x509.Name(
-                [
-                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint TLS Root CA'),
-                ]
-            )
-        )
-        builder = builder.issuer_name(
-            x509.Name(
-                [
-                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint TLS Root CA'),
-                ]
-            )
-        )
-        builder = builder.not_valid_before(datetime.datetime.now(datetime.UTC) - ONE_DAY)
-        builder = builder.not_valid_after(datetime.datetime.now(datetime.UTC) + (4 * 365 * ONE_DAY))
-        builder = builder.serial_number(x509.random_serial_number())
-        builder = builder.public_key(public_key)
-        builder = builder.add_extension(x509.SubjectKeyIdentifier.from_public_key(public_key), critical=False)
-        builder = builder.add_extension(x509.AuthorityKeyIdentifier.from_issuer_public_key(public_key), critical=False)
-        builder = builder.add_extension(x509.BasicConstraints(ca=True, path_length=1), critical=True)
-        builder = builder.add_extension(
-            x509.KeyUsage(
-                digital_signature=True,
-                content_commitment=False,
-                key_encipherment=False,
-                data_encipherment=False,
-                key_agreement=False,
-                key_cert_sign=True,
-                crl_sign=True,
-                encipher_only=False,
-                decipher_only=False,
-            ),
-            critical=True,
-        )
-        root_ca_certificate = builder.sign(
-            private_key=private_key,
-            algorithm=hashes.SHA256(),
-        )
-
-        return CredentialSerializer((private_key, root_ca_certificate, None))
-
-    def _generate_issuing_ca_credential(self, root_ca_credential: CredentialSerializer) -> CredentialSerializer:
-        private_key_serializer = self._generate_key_pair()
-        private_key = private_key_serializer.as_crypto()
-        public_key = private_key_serializer.public_key_serializer.as_crypto()
-
-        builder = x509.CertificateBuilder()
-        builder = builder.subject_name(
-            x509.Name(
-                [
-                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint TLS Issuing CA'),
-                ]
-            )
-        )
-        builder = builder.issuer_name(
-            x509.Name(
-                [
-                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint TLS Root CA'),
-                ]
-            )
-        )
-        builder = builder.not_valid_before(datetime.datetime.now(datetime.UTC) - ONE_DAY)
-        builder = builder.not_valid_after(datetime.datetime.now(datetime.UTC) + (2 * 365 * ONE_DAY))
-        builder = builder.serial_number(x509.random_serial_number())
-        builder = builder.public_key(public_key)
-        builder = builder.add_extension(x509.SubjectKeyIdentifier.from_public_key(public_key), critical=False)
-        builder = builder.add_extension(x509.AuthorityKeyIdentifier.from_issuer_public_key(public_key), critical=False)
-        builder = builder.add_extension(x509.BasicConstraints(ca=True, path_length=0), critical=True)
-        builder = builder.add_extension(
-            x509.KeyUsage(
-                digital_signature=True,
-                content_commitment=False,
-                key_encipherment=False,
-                data_encipherment=False,
-                key_agreement=False,
-                key_cert_sign=True,
-                crl_sign=True,
-                encipher_only=False,
-                decipher_only=False,
-            ),
-            critical=True,
-        )
-        issuing_ca_certificate = builder.sign(
-            private_key=root_ca_credential.credential_private_key.as_crypto(),
-            algorithm=hashes.SHA256(),
-        )
-
-        return CredentialSerializer(
-            (private_key, issuing_ca_certificate, [root_ca_credential.credential_certificate.as_crypto()])
-        )
-
-    def _generate_tls_server_credential(self, issuing_ca_credential: CredentialSerializer) -> CredentialSerializer:
+    def generate_tls_credential(self) -> CredentialSerializer:
+        """Generates a self-signed TLS credential for use by the Trustpoint Apache server."""
         one_day = datetime.timedelta(1, 0, 0)
         private_key = ec.generate_private_key(curve=ec.SECP256R1())
         public_key = private_key.public_key()
@@ -133,12 +36,16 @@ class Generator:
         builder = builder.subject_name(
             x509.Name(
                 [
-                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint Self-Signed TLS-Server Credential'),
+                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint Self-Signed TLS Server Credential'),
                 ]
             )
         )
         builder = builder.issuer_name(
-            issuing_ca_credential.credential_certificate.as_crypto().subject
+            x509.Name(
+                [
+                    x509.NameAttribute(NameOID.COMMON_NAME, 'Trustpoint Self-Signed TLS Server Credential'),
+                ]
+            )
         )
         builder = builder.not_valid_before(datetime.datetime.now(datetime.UTC) - one_day)
         builder = builder.not_valid_after(datetime.datetime.now(datetime.UTC) + (one_day * 365))
@@ -176,22 +83,10 @@ class Generator:
         builder = builder.add_extension(x509.SubjectAlternativeName(san), critical=True)
 
         certificate = builder.sign(
-            private_key=issuing_ca_credential.credential_private_key.as_crypto(),
+            private_key=private_key,
             algorithm=hashes.SHA256(),
         )
 
         return CredentialSerializer(
-            (
-                private_key,
-                certificate,
-                [
-                    issuing_ca_credential.credential_certificate.as_crypto(),
-                    issuing_ca_credential.additional_certificates.as_crypto()[0],
-                ],
-            )
+            (private_key, certificate, [certificate])
         )
-
-    def generate_tls_credential(self) -> CredentialSerializer:
-        root_ca_credential = self._generate_root_ca()
-        issuing_ca_credential = self._generate_issuing_ca_credential(root_ca_credential)
-        return self._generate_tls_server_credential(issuing_ca_credential)
