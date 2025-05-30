3ebad51ae89d07ec0c046ab299b17f568d64e26a
Rohit Bohara <r.bohara@asvin.io>
2025-05-30 16:02:34 +0200

    implement steps and features for domain

---
+++ trustpoint/features/R_103_domain_management.feature
@@ -0,0 +1,36 @@
+Feature: Manage domains via TPC_Web
+  As an admin user
+  I want to create, view, edit, and delete domains
+  So that I can manage domains effectively through the web interface.
+
+  Background:
+    Given the admin user is logged into TPC_Web
+    And the issuing ca with unique name "test_CA" with pkcs12 file exist
+
+  Scenario Outline: Add a new domain
+    When the admin navigates to the "Add new Domain" page
+    And the admin fills in the domain details with <name> and issuing CA "test_CA"
+    And the admin clicks on "Add new Domain"
+    Then the system should display a confirmation message stating "Successfully created domain"
+    And the new domain with <name> and issuing CA "test_CA" should appear in the domain list
+
+    Examples:
+      | name    |
+      | arburg  |
+      | homag   |
+
+  Scenario Outline: Delete an existing domain
+    Given a domain "trustpoint_test" with issuing ca "test_CA" exist
+    When the admin navigates to the "domain list" page
+    And the admin deletes the domain with the name <name>
+    Then the system should display a confirmation message stating "Successfully deleted"
+    And the domain <name> should no longer appear in the domain list
+
+    Examples:
+      | name    |
+      | arburg  |
+      | homag   |
+
+  Scenario: Handle non-existent domains
+    When the admin attempts to view the details of a non-existent domain "NonExistentID"
+    Then the system should display an error message
---
+++ trustpoint/features/steps/r_103_steps.py
@@ -0,0 +1,119 @@
+"""Python steps file for R_001."""
+
+from behave import given, runner, then, when
+from django.middleware.csrf import get_token
+from pki.models import IssuingCaModel, DomainModel
+from devices.models import DeviceModel
+from bs4 import BeautifulSoup
+import os
+
+
+@given('a domain "{domain_name}" with issuing ca "{ca_name}" exist')
+def step_domain_exists(context: runner.Context, domain_name: str, ca_name: str) -> None:  # noqa: ARG001
+    """.
+
+    Args:
+        context: the behave context
+        domain_name: a domain name
+    """
+    issuing_ca = IssuingCaModel.objects.get(unique_name=ca_name)
+    assert issuing_ca.unique_name == ca_name, f"Issuing CA with name {ca_name} not found"
+    
+    domain, created = DomainModel.objects.get_or_create(unique_name=domain_name)
+    assert created, f" Domain creation failed"
+    assert domain.unique_name == domain_name, f" Domain name mismatch: expected '{domain_name}', got '{domain.name}'"
+
+    domain.issuing_ca = issuing_ca
+    domain.save()
+
+    context.domain = domain
+
+@when('the admin fills in the domain details with {name} and issuing CA "{ca_name}"')
+def step_fill_domain_details(context: runner.Context, name: str, ca_name: str) -> None:  # noqa: ARG001
+    """Fills in the device creation form.
+
+    Args:
+        context (runner.Context): Behave context.
+        name (str): The name of the device.
+        serial_number (str): The Serial number of the device.
+    """
+    ca = IssuingCaModel.objects.get(unique_name=ca_name)
+
+    assert ca.unique_name == ca_name, f"Issuing CA {ca_name} doesn't exist"
+    # Prepare POST data
+    context.domain_add_form_data = {
+        'unique_name': name,
+        'issuing_ca': ca.id,
+        'auto_create_new_device': 'off',
+        'allow_username_password_registration': 'on',
+        'allow_idevid_registration': 'off',
+        'domain_credential_auth': 'on',
+        'domain_credential_auth_helptext': 'off',
+        'username_password_auth': 'off',
+        'allow_app_certs_without_domain': 'on'
+    }
+
+@then('the new domain with {name} and issuing CA "{ca_name}" should appear in the domain list')
+def step_device_list(context: runner.Context, name: str, ca_name: str) -> None:  # noqa: ARG001
+    """Verifies that the new device appears in the device list.
+
+    Args:
+        context (runner.Context): Behave context.
+        name (str): The name of the device.
+        ca_name (str): The name of the issuing ca.
+    """
+    soup = BeautifulSoup(context.response.content, "html.parser")
+
+    # Find all <td> elements
+    tds = soup.find_all("td")
+
+    # Get their text content (unescaped and stripped)
+    values = [td.get_text(strip=True) for td in tds]
+
+    assert name in values, f"Domain {name} doesn't exist"
+    assert ca_name in values, f"Issuing CA {ca_name} doesn't exist"
+
+
+@when('the admin deletes the domain with the name {name}')
+def step_delete_domain(context: runner.Context, name: str) -> None:  # noqa: ARG001
+    """Deletes an domain by name.
+
+    Args:
+        context (runner.Context): Behave context.
+        name (str): The name of the domain to be deleted.
+    """
+
+    context.response = context.authenticated_client.get(
+        '/pki/domains/delete/'+ str(context.domain.id),
+        follow=True,
+        HTTP_X_REQUESTED_WITH="XMLHttpRequest"
+    )
+
+    assert context.response.status_code == 200, "Domain delete form submission failed"
+    assert b"Confirm Domain Deletion" in context.response.content
+    context.response = context.authenticated_client.post(f"/pki/domains/delete/{context.domain.id}/", data={}, follow=True)
+    assert context.response.status_code == 200, "Domain deletion response"
+    assert not DomainModel.objects.filter(id=context.domain.id).exists(), "Domain deletion failed"
+
+
+@then('the domain {name} should no longer appear in the domain list')
+def step_verify_domain_deletion(context: runner.Context, name: str) -> None:  # noqa: ARG001
+    """Verifies that the device no longer appears in the list.
+
+    Args:
+        context (runner.Context): Behave context.
+        name (str): The name of the domain.
+    """
+    assert name not in context.response, "Domain still exist in the list"
+
+
+@when('the admin attempts to view the details of a non-existent domain {non_existent_device_id}')
+def step_attempt_view_nonexistent(context: runner.Context, non_existent_device_id: str) -> None:  # noqa: ARG001
+    """Attempts to view details of a non-existent device.
+
+    Args:
+        context (runner.Context): Behave context.
+        non_existent_device_id (str): The id a non-existent device.
+    """
+    #Navigate (GET request) to the device detailed page
+    context.response = context.authenticated_client.get(f"/pki/domains/config/{non_existent_device_id}")
