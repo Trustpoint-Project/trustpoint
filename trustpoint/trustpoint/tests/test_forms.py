aa1dc6e75ae05f6bd3ee74186f8e6b00d0f4bcf0
florianhandke <florian.handke@campus-schwarzwald.de>
2025-06-06 16:34:16 +0200

    Add unit tests for cleaned data validation in `test_forms.py`

--- trustpoint/trustpoint/tests/test_forms.py
+++ trustpoint/trustpoint/tests/test_forms.py
@@ -1,8 +1,12 @@
-import pytest
-from django import forms
+"""Tests for the forms module."""
+
 from typing import Any
 
+import pytest
+from django import forms
+
 from trustpoint.forms import CleanedDataNotNoneMixin
+
 
 class DummyForm(CleanedDataNotNoneMixin, forms.Form):
     field = forms.CharField(required=True)
@@ -13,12 +17,12 @@ class TestCleanedDataNotNoneMixin:
 
     def test_clean_returns_cleaned_data(self) -> None:
         """Verify clean method returns valid cleaned_data."""
-        data = {"field": "test"}
+        data = {'field': 'test'}
         form = DummyForm(data=data)
         assert form.is_valid()
         cleaned_data = form.clean()
         assert isinstance(cleaned_data, dict)
-        assert cleaned_data["field"] == "test"
+        assert cleaned_data['field'] == 'test'
 
     def test_clean_raises_error_for_empty_data(self) -> None:
         """Check clean raises ValidationError if data is invalid."""
@@ -30,5 +34,5 @@ class TestCleanedDataNotNoneMixin:
                 return super().clean()
 
         form = TestForm(data={})
-        with pytest.raises(forms.ValidationError, match="Failed to get cleaned form data."):
+        with pytest.raises(forms.ValidationError, match='Failed to get cleaned form data.'):
             form.clean()
