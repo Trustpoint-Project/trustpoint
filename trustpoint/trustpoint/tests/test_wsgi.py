c5293552943d2007d071288efe0ea2bdb9934751
florianhandke <florian.handke@campus-schwarzwald.de>
2025-06-06 16:35:23 +0200

    Refactor and update tests for WSGI configuration in `test_wsgi.py`

--- trustpoint/trustpoint/tests/test_wsgi.py
+++ trustpoint/trustpoint/tests/test_wsgi.py
@@ -1,20 +1,23 @@
+"""Tests for the WSGI configuration module."""
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 from trustpoint.wsgi import application
 
 
-def test_wsgi_application_initialization():
+def test_wsgi_application_initialization() -> None:
     """Test that the WSGI application is initialized correctly."""
-
-    assert os.getenv("DJANGO_SETTINGS_MODULE") == "trustpoint.settings", \
+    assert os.getenv('DJANGO_SETTINGS_MODULE') == 'trustpoint.settings', \
         "DJANGO_SETTINGS_MODULE should be 'trustpoint.settings'."
 
-    assert callable(application), "WSGI application should be callable."
+    assert callable(application), 'WSGI application should be callable.'
 
     try:
         wsgi_app = get_wsgi_application()
         assert callable(wsgi_app), (
-            "Initializing the WSGI application with `get_wsgi_application` should produce a callable."
+            'Initializing the WSGI application with `get_wsgi_application` should produce a callable.'
         )
     except Exception as e:
-        raise AssertionError(f"WSGI application initialization failed. Error: {e}")
+        error_message = f'WSGI application initialization failed. Error: {e}'
+        raise AssertionError(error_message)from e
