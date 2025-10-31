c5da924f4949bad3d56fa0852e6201e2d381681c
AlexHx8472 <alex_mail@posteo.de>
2025-10-31 01:11:29 +0100

    deactivated demo data injection

--- docker/trustpoint/entrypoint.sh
+++ docker/trustpoint/entrypoint.sh
@@ -12,10 +12,10 @@ until pg_isready -h "$DATABASE_HOST" -p "$DATABASE_PORT" &>/dev/null; do
 done
 echo "PostgreSQL database is available!"
 
-run_as_www_data "uv run trustpoint/manage.py reset_db --force"
-run_as_www_data "uv run trustpoint/manage.py inittrustpoint"
-run_as_www_data "uv run trustpoint/manage.py tls_cred"
-run_as_www_data "uv run trustpoint/manage.py add_domains_and_devices"
+# run_as_www_data "uv run trustpoint/manage.py reset_db --force"
+# run_as_www_data "uv run trustpoint/manage.py inittrustpoint"
+# run_as_www_data "uv run trustpoint/manage.py tls_cred"
+# run_as_www_data "uv run trustpoint/manage.py add_domains_and_devices"
 
 run_as_www_data "uv run trustpoint/manage.py managestartup"
 
