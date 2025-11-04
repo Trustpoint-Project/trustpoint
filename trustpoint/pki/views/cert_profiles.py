d11bf2ae43eb8169e76529bf10e8ea81c0139001
Christian Schwinne <cschwinne@gmail.com>
2025-11-04 11:57:24 +0100

    Basic profile editing

--- trustpoint/pki/views/cert_profiles.py
+++ trustpoint/pki/views/cert_profiles.py
@@ -2,17 +2,18 @@
 
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING, Any
 
 from django.contrib import messages
-from django.core.exceptions import ValidationError
 from django.db.models import ProtectedError, QuerySet
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
+from pydantic import ValidationError
 from trustpoint.logger import LoggerMixin
 from trustpoint.views.base import (
     BulkDeleteView,
@@ -22,6 +23,7 @@ from trustpoint.views.base import (
 
 from pki.forms import OwnerCredentialFileImportForm
 from pki.models import CertificateProfileModel
+from pki.util.cert_profile import CertProfileModel as CertProfilePydanticModel
 from trustpoint.settings import UIConfig
 
 if TYPE_CHECKING:
@@ -48,7 +50,7 @@ class CertProfileTableView(CertProfileContextMixin, SortableTableMixin, ListView
 class CertProfileConfigView(LoggerMixin, CertProfileContextMixin, DetailView[CertificateProfileModel]):
     """View to display the details of and edit a Certificate Profile."""
 
-    http_method_names = ('get',)
+    http_method_names = ('get', 'post')
 
     model = CertificateProfileModel
     success_url = reverse_lazy('pki:cert_profiles')
@@ -69,8 +71,34 @@ class CertProfileConfigView(LoggerMixin, CertProfileContextMixin, DetailView[Cer
         context = super().get_context_data(**kwargs)
         cert_profile = self.get_object()
         context['profile'] = cert_profile
+        #pretty_json = json.dumps(cert_profile.profile_json, indent=2)
+        context['profile_json'] = json.loads(cert_profile.profile_json)
 
         return context
+
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        """Handles POST requests to the view.
+
+        Args:
+            request: The HTTP request object.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            An HTTP response redirecting to the success URL.
+        """
+        del args, kwargs  # Unused
+        try:
+            cert_profile = self.get_object()
+            json_dict = json.loads(request.POST.get('profile_json'))
+            #JSONProfileVerifier(json_dict)
+            CertProfilePydanticModel.model_validate(json_dict)
+            cert_profile.profile_json = json.dumps(json_dict)
+            cert_profile.save()
+            messages.success(request, _('Certificate Profile updated successfully.'))
+        except ValidationError as exc:
+            messages.error(request, _('Error updating Certificate Profile: ') + str(exc))
+        return HttpResponseRedirect(self.success_url)
 
 
 class CertProfileAddView(CertProfileContextMixin, FormView[OwnerCredentialFileImportForm]):
