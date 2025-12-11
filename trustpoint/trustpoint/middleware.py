ccd1de1dab3a3bda2b1e07a52b10f4ce09001387
florianhandke <florian.handke@campus-schwarzwald.de>
2025-12-11 09:23:11 +0100

    fixed failing tests

--- trustpoint/trustpoint/middleware.py
+++ trustpoint/trustpoint/middleware.py
@@ -10,7 +10,7 @@ from django.contrib.auth.middleware import LoginRequiredMiddleware
 if TYPE_CHECKING:
     from collections.abc import Callable
 
-    from django.http import HttpRequest, HttpResponse
+    from django.http import HttpRequest, HttpResponseBase
 
 
 # TODO(AlexHx8472): Stubs not yet available in django-stubs.  # noqa: FIX002
@@ -24,7 +24,7 @@ class TrustpointLoginRequiredMiddleware(LoginRequiredMiddleware):
         view_func: Callable[..., Any],
         view_args: tuple[Any, ...],
         view_kwargs: dict[str, Any],
-    ) -> None | HttpResponse:
+    ) -> None | HttpResponseBase:
         """Allow unauthenticated access to public paths, else redirect to login page."""
         if not request.user.is_authenticated and any(request.path.startswith(path) for path in settings.PUBLIC_PATHS):
             return None
