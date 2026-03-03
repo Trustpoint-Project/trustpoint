bb8b9343fcf149ffa6e02f2df99e76f2c7432fe8
florianhandke <florian.handke@campus-schwarzwald.de>
2026-03-03 13:43:16 +0100

    reuse existing session

--- trustpoint/management/models/pkcs11.py
+++ trustpoint/management/models/pkcs11.py
@@ -291,14 +291,10 @@ class PKCS11Token(models.Model, LoggerMixin):
             try:
                 session = pkcs11_token.open(user_pin=self.get_pin(), rw=True)
             except pkcs11.UserAlreadyLoggedIn:
-                # If user is already logged in, logout and try again
-                self.logger.debug('User already logged in to token %s, logging out and retrying', self.label)
-                try:
-                    pkcs11_token.logout()
-                except Exception as logout_error:  # noqa: BLE001
-                    self.logger.warning('Failed to logout before reopening session: %s', logout_error)
-                # Retry opening the session
-                session = pkcs11_token.open(user_pin=self.get_pin(), rw=True)
+                # If user is already logged in, reuse the existing session without logging out
+                # This avoids unnecessary session disruption
+                self.logger.debug('User already logged in to token %s, reusing existing session', self.label)
+                session = pkcs11_token.open(rw=True)
 
             wrap_key = session.get_key(
                 key_type=pkcs11.KeyType.AES,
@@ -553,12 +549,7 @@ class PKCS11Token(models.Model, LoggerMixin):
         try:
             session = pkcs11_token.open(user_pin=self.get_pin(), rw=True)
         except pkcs11.UserAlreadyLoggedIn:
-            self.logger.debug('User already logged in to token %s, logging out and retrying', self.label)
-            try:
-                pkcs11_token.logout()
-            except Exception as logout_error:  # noqa: BLE001
-                self.logger.warning('Failed to logout before reopening session: %s', logout_error)
-            session = pkcs11_token.open(user_pin=self.get_pin(), rw=True)
+            session = pkcs11_token.open(rw=True)
 
         wrap_key = session.get_key(
             key_type=pkcs11.KeyType.AES,
