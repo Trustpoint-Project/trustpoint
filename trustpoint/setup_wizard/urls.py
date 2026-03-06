801b5f096263bcbf654cca7d8b7b5da3c345c4bd
AlexHx8472 <alex_mail@posteo.de>
2026-03-06 10:47:50 +0100

    fresh install wip

--- trustpoint/setup_wizard/urls.py
+++ trustpoint/setup_wizard/urls.py
@@ -48,51 +48,51 @@ urlpatterns = [
     #     name='auto_restore_hsm_setup',
     # ),
     # path('setup_mode/', SetupWizardSetupModeView.as_view(), name='setup_mode'),
-    # path(
-    #     'select_tls_server_credential/',
-    #     SetupWizardSelectTlsServerCredentialView.as_view(),
-    #     name='select_tls_server_credential',
-    # ),
-    # path(
-    #     'import-tls-server-credential/',
-    #     SetupWizardImportTlsServerCredentialMethodSelectView.as_view(),
-    #     name='import_tls_server_credential',
-    # ),
-    # path(
-    #     'import-tls-server-credential/pkcs12/',
-    #     SetupWizardImportTlsServerCredentialPkcs12View.as_view(),
-    #     name='import_tls_server_credential_pkcs12',
-    # ),
-    # path(
-    #     'import-tls-server-credential/separate-files/',
-    #     SetupWizardImportTlsServerCredentialSeparateFilesView.as_view(),
-    #     name='import_tls_server_credential_separate_files',
-    # ),
+    path(
+        'fresh-install/select_tls_server_credential/',
+        SetupWizardSelectTlsServerCredentialView.as_view(),
+        name='select_tls_server_credential',
+    ),
+    path(
+        'fresh-install/import-tls-server-credential/',
+        SetupWizardImportTlsServerCredentialMethodSelectView.as_view(),
+        name='import_tls_server_credential',
+    ),
+    path(
+        'fresh-install/import-tls-server-credential/pkcs12/',
+        SetupWizardImportTlsServerCredentialPkcs12View.as_view(),
+        name='import_tls_server_credential_pkcs12',
+    ),
+    path(
+        'fresh-install/import-tls-server-credential/separate-files/',
+        SetupWizardImportTlsServerCredentialSeparateFilesView.as_view(),
+        name='import_tls_server_credential_separate_files',
+    ),
     # path(
     #     'backup-password/',
     #     SetupWizardBackupPasswordView.as_view(),
     #     name='backup_password',
     # ),
-    # path(
-    #     'generate-tls-server-credential/',
-    #     SetupWizardGenerateTlsServerCredentialView.as_view(),
-    #     name='generate_tls_server_credential',
-    # ),
-    # path(
-    #     'tls-server-credential-apply/',
-    #     SetupWizardTlsServerCredentialApplyView.as_view(),
-    #     name='tls_server_credential_apply',
-    # ),
-    # path(
-    #     'tls-server-credential-apply/<str:file_format>/',
-    #     SetupWizardTlsServerCredentialApplyView.as_view(),
-    #     name='tls_server_credential_apply',
-    # ),
-    # path(
-    #     'tls-server-credential-apply-cancel/',
-    #     SetupWizardTlsServerCredentialApplyCancelView.as_view(),
-    #     name='tls_server_credential_apply_cancel',
-    # ),
+    path(
+        'fresh-install/generate-tls-server-credential/',
+        SetupWizardGenerateTlsServerCredentialView.as_view(),
+        name='generate_tls_server_credential',
+    ),
+    path(
+        'fresh-install/tls-server-credential-apply/',
+        SetupWizardTlsServerCredentialApplyView.as_view(),
+        name='tls_server_credential_apply',
+    ),
+    path(
+        'fresh-install/tls-server-credential-apply/<str:file_format>/',
+        SetupWizardTlsServerCredentialApplyView.as_view(),
+        name='tls_server_credential_apply',
+    ),
+    path(
+        'fresh-install/tls-server-credential-apply-cancel/',
+        SetupWizardTlsServerCredentialApplyCancelView.as_view(),
+        name='tls_server_credential_apply_cancel',
+    ),
     # path('demo-data/', SetupWizardDemoDataView.as_view(), name='demo_data'),
     # path('create-super-user', SetupWizardCreateSuperUserView.as_view(), name='create_super_user'),
     # path('restore/', BackupRestoreView.as_view(), name='restore'),
