cf43096c7ea09022c49b929f1b287bbe01cd3c3d
Christian Schwinne <cschwinne@gmail.com>
2026-03-19 19:52:15 +0100

    mDNS fixes

--- docker/mdns/entrypoint.sh
+++ docker/mdns/entrypoint.sh
@@ -25,6 +25,7 @@ fi;
 AUTO_IP=$(ip route get 1.1.1.1 | grep -o 'src [0-9\.]*' | cut -d' ' -f2)
 
 if [ -z "$AUTO_IP" ]; then
+    # 1st Fallback: Default gateway (offline deployment)
     echo "[WARN] Could not detect Host IP. Using fallback gateway detection."
     GATEWAY_IFACE=$(ip route | grep default | awk '{print $5}' | head -n1)
 
@@ -51,25 +52,25 @@ if [ -S /var/run/avahi-daemon/socket ]; then
     echo "[INFO] Found existing Avahi socket on host. Using host daemon."
 else
     echo "[INFO] No host Avahi found. Starting internal D-Bus and Avahi..."
-    
+
     # Ensure D-Bus is ready
     rm -f /var/run/dbus/pid
     rm -f /run/dbus/dbus.pid
     dbus-uuidgen --ensure
     #dbus-daemon --system --fork
     dbus-daemon --system --fork --nopidfile --print-address
-    
+
     # Wait for the socket to actually appear
     until [ -S /var/run/dbus/system_bus_socket ]; do
         echo "[INFO] Waiting for D-Bus socket..."
         sleep 0.5
     done
-    
+
     # Start Avahi in the background
     #avahi-daemon -D --no-chroot
     echo "[INFO] Starting Avahi-daemon..."
     avahi-daemon --no-chroot --no-proc-title
-    
+
     until [ -S /var/run/avahi-daemon/socket ]; do
         echo "[INFO] Waiting for Avahi socket..."
         sleep 0.5
@@ -79,12 +80,13 @@ fi
 # 3. Launch Advertisements
 # We use & to background them so we can track them all
 echo "[INFO] Publishing address and services -> $AUTO_IP"
-avahi-publish -a trustpoint.local "$AUTO_IP" &
+
+avahi-publish -a -R trustpoint.local "$AUTO_IP" &
 
 avahi-publish -s 'Trustpoint Web Interface' _https._tcp 443 &
-avahi-publish -s 'AOKI Owner Service' _aoki._tcp 443
+avahi-publish -s 'AOKI Owner Service' _aoki._tcp 443 &
 
 echo "[INFO] mDNS services published. Container is running."
 
-# 4. Fail fast if a background process dies
+
 wait -n
