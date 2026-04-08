62301f77f9fe70381300d0409fc5a91ba865d602
AlexHx8472 <alex_mail@posteo.de>
2026-04-08 07:56:31 +0200

    removed state file parts

--- docker/trustpoint/wizard/wizard_restore.sh
+++ docker/trustpoint/wizard/wizard_restore.sh
@@ -20,34 +20,8 @@ log() {
 # 4) Trap unexpected errors and log them
 trap 'log ERROR "in ${FUNCNAME[0]:-main} at line $LINENO (exit=$?)"; exit 1' ERR
 
-# ————— Paths —————
-STATE_DIR="/etc/trustpoint/wizard/state"
-WIZARD_SETUP_MODE="$STATE_DIR/WIZARD_SETUP_MODE"
-WIZARD_COMPLETED="$STATE_DIR/WIZARD_COMPLETED"
-
 
 log INFO "STARTING NGINX RESTORE"
-
-# 5) Check wizard state
-log INFO "Checking for state file $WIZARD_SETUP_MODE"
-if [ ! -f "$WIZARD_SETUP_MODE" ]; then
-  log ERROR "State file $WIZARD_SETUP_MODE not found"
-  exit 1
-fi
-
-# Removes the current WIZARD_SETUP_MODE state file.
-if ! rm "$WIZARD_SETUP_MODE"
-then
-    log "ERROR: Failed to remove the WIZARD_SETUP_MODE state file."
-    exit 3
-fi
-
-# Creates the WIZARD_COMPLETED state file.
-if ! touch "$WIZARD_COMPLETED"
-then
-    log "ERROR: Failed to create the WIZARD_COMPLETED state file."
-    exit 4
-fi
 
 # 12) if Nginx is already running, update tls and gracefully restart
 log INFO "DEBUG: Checking if nginx is running"
