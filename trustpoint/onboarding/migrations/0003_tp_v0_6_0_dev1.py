a7c94928387a614dc6a7136e29461b5ed5dbcd63
ChrisSchi <christof.schillinger@campus-schwarzwald.de>
2026-05-29 13:11:24 +0200

    CHANGE: Cause codecov doesn't work

---
+++ trustpoint/onboarding/migrations/0003_tp_v0_6_0_dev1.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('onboarding', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='onboardingconfigmodel',
+            name='onboarding_protocol',
+            field=models.PositiveIntegerField(choices=[(0, 'Manual Onboarding'), (1, 'CMP - IDevID'), (2, 'CMP - Shared Secret'), (3, 'EST - IDevID'), (4, 'EST - Username & Password'), (5, 'AOKI'), (6, 'BRSKI'), (7, 'OPC - GDS Push'), (8, 'REST - Username & Password')], verbose_name='Onboarding Protocol'),
+        ),
+    ]
