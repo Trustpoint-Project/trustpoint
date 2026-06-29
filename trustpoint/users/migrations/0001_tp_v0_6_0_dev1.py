a5bae6d10c4ebd707bfd96996b86cf62faa24600
Rohit Bohara <r.bohara@asvin.io>
2026-06-29 11:45:01 +0200

    change migrations

--- trustpoint/users/migrations/0001_tp_v0_6_0_dev1.py
+++ trustpoint/users/migrations/0001_tp_v0_6_0_dev1.py
@@ -11,6 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
+        ('management', '0003_tp_v0_6_0_dev1'),
     ]
 
     operations = [
@@ -39,6 +40,7 @@ class Migration(migrations.Migration):
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('organization', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='org_users', to='management.organizationmodel', verbose_name='organization')),
                 ('role', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='trustpoint_users', to='auth.group', verbose_name='role')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
             ],
