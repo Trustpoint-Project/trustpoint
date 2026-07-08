8a1fb59c50995db3b58b3841ba8213430475876b
florianhandke <florian.handke@campus-schwarzwald.de>
2026-07-08 14:41:02 +0200

    migrations

--- trustpoint/pki/migrations/0002_tp_v0_6_0_dev1.py
+++ trustpoint/pki/migrations/0002_tp_v0_6_0_dev1.py
@@ -1,4 +1,6 @@
 import django.db.models.deletion
+import trustpoint.logger
+from django.conf import settings
 from django.db import migrations, models
 
 
@@ -8,9 +10,29 @@ class Migration(migrations.Migration):
         ('crypto', '0001_tp_v0_6_0_dev1'),
         ('management', '0003_tp_v0_6_0_dev1'),
         ('pki', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='CaRolloverModel',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('state', models.CharField(choices=[('planned', 'Planned'), ('awaiting_new_ca', 'Awaiting New CA'), ('preparation', 'Preparation'), ('transition', 'Transition'), ('completed', 'Completed'), ('cancelled', 'Cancelled')], default='planned', max_length=20, verbose_name='State')),
+                ('strategy_type', models.CharField(choices=[('import_ca', 'Import new Issuing CA from file'), ('generate_keypair', 'Generate keypair and request certificate'), ('remote_ca', 'Configure a remote Issuing CA')], help_text='How the new Issuing CA is provisioned.', max_length=20, verbose_name='Strategy')),
+                ('planned_at', models.DateTimeField(auto_now_add=True, verbose_name='Planned At')),
+                ('started_at', models.DateTimeField(blank=True, null=True, verbose_name='Started At')),
+                ('completed_at', models.DateTimeField(blank=True, null=True, verbose_name='Completed At')),
+                ('transition_scheduled_at', models.DateTimeField(blank=True, help_text='When the rollover should automatically move from Preparation to Transition phase.', null=True, verbose_name='Scheduled Transition Time')),
+                ('notes', models.TextField(blank=True, default='', verbose_name='Notes')),
+            ],
+            options={
+                'verbose_name': 'CA Rollover',
+                'verbose_name_plural': 'CA Rollovers',
+                'ordering': ['-planned_at'],
+            },
+            bases=(trustpoint.logger.LoggerMixin, models.Model),
+        ),
         migrations.AlterUniqueTogether(
             name='pkcs11key',
             unique_together=None,
@@ -63,7 +85,26 @@ class Migration(migrations.Migration):
             model_name='revokedcertificatemodel',
             index=models.Index(fields=['ca', 'revoked_at'], name='pki_revoked_ca_revoked_at_idx'),
         ),
+        migrations.AddField(
+            model_name='carollovermodel',
+            name='initiated_by',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='Initiated By'),
+        ),
+        migrations.AddField(
+            model_name='carollovermodel',
+            name='new_issuing_ca',
+            field=models.ForeignKey(blank=True, help_text='The replacement Issuing CA. Null until the new CA is ready.', null=True, on_delete=django.db.models.deletion.PROTECT, related_name='rollovers_as_new', to='pki.camodel', verbose_name='New Issuing CA'),
+        ),
+        migrations.AddField(
+            model_name='carollovermodel',
+            name='old_issuing_ca',
+            field=models.ForeignKey(help_text='The Issuing CA being replaced.', on_delete=django.db.models.deletion.PROTECT, related_name='rollovers_as_old', to='pki.camodel', verbose_name='Old Issuing CA'),
+        ),
         migrations.DeleteModel(
             name='PKCS11Key',
+        ),
+        migrations.AddConstraint(
+            model_name='carollovermodel',
+            constraint=models.UniqueConstraint(condition=models.Q(('state__in', ['planned', 'awaiting_new_ca', 'preparation', 'transition'])), fields=('old_issuing_ca',), name='unique_active_rollover_per_old_ca'),
         ),
     ]
