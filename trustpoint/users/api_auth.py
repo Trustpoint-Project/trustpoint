4b4196e2684ea2c51bc68292eff005ae29107f26
florianhandke <florian.handke@campus-schwarzwald.de>
2026-07-28 11:46:47 +0200

    fix copyright tag

--- trustpoint/users/api_auth.py
+++ trustpoint/users/api_auth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 The Trustpoint Project Authors
+# Copyright (c) 2026 The Trustpoint Project Authors
 # SPDX-License-Identifier: MIT
 
 """REST Framework authentication for service accounts."""
--- trustpoint/users/management/commands/create_service_account.py
+++ trustpoint/users/management/commands/create_service_account.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 The Trustpoint Project Authors
+# Copyright (c) 2026 The Trustpoint Project Authors
 # SPDX-License-Identifier: MIT
 
 """Management command to create service accounts and API keys."""
