fc0a6da68dfadd3fe8a6719e02d0f29306670f22
florianhandke <florian.handke@campus-schwarzwald.de>
2026-07-28 09:25:37 +0200

    update

--- trustpoint/management/views/health.py
+++ trustpoint/management/views/health.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 The Trustpoint Project Authors
+# Copyright (c) 2026 The Trustpoint Project Authors
 # SPDX-License-Identifier: MIT
 
 """Health check views for monitoring Trustpoint Extension status."""
