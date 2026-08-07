dc30dabffe09185a90c5cea09ac8a3f352792909
ChrisSchi <christof.schillinger@campus-schwarzwald.de>
2026-08-07 14:57:00 +0200

    ADD: API Endpoints for user and role management

--- trustpoint/management/api_urls.py
+++ trustpoint/management/api_urls.py
@@ -6,11 +6,15 @@ from rest_framework.routers import DefaultRouter
 
 from management.views.backup import BackupViewSet
 from management.views.logging import LoggingViewSet
+from management.views.role_management import RoleViewSet
 from management.views.tls import TlsViewSet
+from management.views.user_management import UserViewSet
 
 router = DefaultRouter()
 router.register(r'backups', BackupViewSet, basename='backup')
 router.register(r'logging', LoggingViewSet, basename='logging')
 router.register(r'tls', TlsViewSet, basename='tls')
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'roles', RoleViewSet, basename='roles')
 
 urlpatterns = router.urls
